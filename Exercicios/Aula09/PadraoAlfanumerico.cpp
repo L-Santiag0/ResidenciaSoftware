1a074c3d34dfcf806f936049284070549f07f86a
Lucas Santiago <lucascarmo2811@gmail.com>
2023-09-05 10:58:18 -0300

    Exercício que imprime o padrão alfanúmerico

---
+++ Exercicios/Aula09/PadraoAlfanumerico.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char alfabetoUp = 'A', alfabetoLower = 'a', numeros = '0';    int altura;
+	
+	cout << "Digite a quantidade de linhas: ";
+	cin >> altura;
+	for (int coluna = 1 ; coluna <= altura ; coluna++){
+	    for (int linha = 1 ; linha <= coluna ; linha++){
+	           if (alfabetoUp <= 'Z'){
+    	            cout << alfabetoUp;
+    	            alfabetoUp += 1;
+	           }
+	           else if (alfabetoLower <= 'z'){
+	                cout << alfabetoLower;
+	                alfabetoLower += 1;
+	           }
+	          else if (numeros <= '9'){
+	              cout << numeros;
+	              numeros += 1;
+	           }
+	          else {
+	              cout << "*";
+	          }
+	      }
+	cout << endl;
+	}
+}
