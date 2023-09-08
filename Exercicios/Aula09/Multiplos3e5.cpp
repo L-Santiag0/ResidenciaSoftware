98a399e279d8789328c232c81b7e6f5862374b0d
Lucas Santiago <lucascarmo2811@gmail.com>
2023-09-07 21:19:31 -0300

    Adicionando descrição do exercício

--- Exercicios/Aula09/Multiplos3e5.cpp
+++ Exercicios/Aula09/Multiplos3e5.cpp
@@ -1,3 +1,10 @@
+/*
+    Escreva um programa em C++ que imprima na tela os números de 1 a 100.
+    Porém, para múltiplos de 3, o programa deve imprimir "Fizz", e para
+    múltiplos de 5, deve imprimir "Buzz". Para números que são múltiplos de
+    ambos, imprimir "FizzBuzz".
+*/
+
 #include <iostream>
 
 using namespace std;
