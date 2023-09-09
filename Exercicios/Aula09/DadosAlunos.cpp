5531b42a61ade96abdf101c360ecbef01a1c8d1f
Lucas Santiago <lucascarmo2811@gmail.com>
2023-09-09 20:27:34 -0300

    Adicionando valor a partir da posição 0.

--- Exercicios/Aula09/DadosAlunos.cpp
+++ Exercicios/Aula09/DadosAlunos.cpp
@@ -8,7 +8,7 @@
     =========================================
     123456789   10.0    8.0     9.9     9.3
 
-    OBS. (para mim): O código está funcionando, porém, é uma gambiarra. Lembre de cosertar!
+    OBS. (para mim): O código está funcionando, porém, lembre de consertar!
 */
 
 #include <iostream>
@@ -26,23 +26,23 @@ int main(){
 
     do {
         cout << "Qual o número da matrícula? ";
-        cin >> matricula[contador];
+        cin >> matricula[contador-1];
         cout << "Informe a primeira nota: ";
-        cin >> nota1[contador];
+        cin >> nota1[contador-1];
         cout << "Informe a segunda nota: ";
-        cin >> nota2[contador];
+        cin >> nota2[contador-1];
         cout << "Informe a terceira nota: ";
-        cin >> nota3[contador];
+        cin >> nota3[contador-1];
         cout << "Deseja cadastrar um novo aluno? [Y/N] ";
         cin >> novoCadastro;
         cout << endl;
 
-        media[contador] = ((nota1[contador] + nota2[contador] + nota3[contador]) / 3);
+        media[contador-1] = ((nota1[contador-1] + nota2[contador-1] + nota3[contador-1]) / 3);
         contador += 1;
 
     } while (toupper(novoCadastro) == 'Y');
 
-    for (int i = 1 ; i < contador ; i++){
+    for (int i = 0 ; i < contador-1 ; i++){
         cout << "\nMATRÍCULA\tNOTA1\tNOTA2\tNOTA3\tMÉDIA\n";
         cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
         cout << matricula[i] << "\t\t" << nota1[i] << "\t" << nota2[i] << "\t" << nota3[i] << "\t" << media[i] << endl;
